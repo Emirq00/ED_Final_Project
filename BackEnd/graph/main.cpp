95715ec94c95211d14ec71b64c75fe25180103fd
evelin-jimenez1 <143577003+evelin-jimenez1@users.noreply.github.com>
2025-06-02 05:22:35 -0600

    Add files via upload

--- BackEnd/graph/main.cpp
+++ BackEnd/graph/main.cpp
@@ -12,54 +12,6 @@ int main() {
     
     httplib::Server svr;
     
-<<<<<<< HEAD
-<<<<<<< HEAD
-svr.Options("/ruta", [](const httplib::Request&, httplib::Response& res) {
-    res.set_header("Access-Control-Allow-Origin", "*");
-    res.set_header("Access-Control-Allow-Methods", "POST, OPTIONS");
-    res.set_header("Access-Control-Allow-Headers", "Content-Type");
-    res.status = 204;
-});
-
-svr.Post("/ruta", [](const httplib::Request& req, httplib::Response& res) {
-        try {
-            auto body = json::parse(req.body);
-            string origen = body["origen"];
-            string destino = body["destino"];
-
-            if (m.find(origen) == m.end() || m.find(destino) == m.end()) {
-                res.status = 400;
-                res.set_header("Access-Control-Allow-Origin", "*");
-                res.set_content("Estación no válida", "text/plain");
-                return;
-=======
-    cout << "Ingrese la estacion en la que se encuentra: ";
-    cin >> station;
-    if(m.find(station) == m.end()) {
-        cout << "estacion no valida";
-        return -1;
-    }
-    
-    shortest_routes = dijkstra(92, m[station], adj);
-    
-    cout << "A que estacion quieres llegar?: ";
-    cin >> station2;
-    if(m.find(station2) == m.end()) {
-        cout << "estacion no valida";
-        return -1;
-    }
-    
-    for(auto i : shortest_routes[m[station2]]) {
-        for(auto &j : m) {
-            if(j.second == i) {
-                cout << j.first << " - ruta(s)[";
-                vector<int> rt = routes[j.first];
-                for(auto i : rt) {
-                    cout << i << ", ";
-                }
-                cout << "]\n";
->>>>>>> c2488e8707e2276e8055fba152831e42071dd888
-=======
     svr.Options("/ruta", [](const httplib::Request&, httplib::Response& res) {
         res.set_header("Access-Control-Allow-Origin", "*");
         res.set_header("Access-Control-Allow-Methods", "POST, OPTIONS");
@@ -79,7 +31,6 @@ svr.Post("/ruta", [](const httplib::Request& req, httplib::Response& res) {
                 res.set_header("Access-Control-Allow-Origin", "*");
                 res.set_content("Estación no válida", "text/plain");
                 return;
->>>>>>> 8497bba861173970a89f6c3cd5efc0a0c17016a8
             }
 
 
